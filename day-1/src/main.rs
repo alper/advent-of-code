ec0317f2ee1f15bcf7a4593fad6d571061f41018
Alper Çugun <github@alper.nl>
2020-12-02 23:34:26 +0100

    Initial commit

---
+++ day-1/src/main.rs
@@ -0,0 +1,50 @@
+use std::fs;
+
+fn main() {
+    let mut input: Vec<&str> = r"1721
+    979
+    366
+    299
+    675
+    1456"
+        .lines()
+        .collect();
+
+    let real_input = fs::read_to_string("input/first.txt").expect("File dead");
+
+    input = real_input.lines().collect();
+
+    let target = 2020;
+
+    for pos1 in 0..input.len() {
+        for pos2 in (0..input.len()).skip(pos1 + 1) {
+            let one = input[pos1].trim().parse::<i32>().unwrap();
+            let other = input[pos2].trim().parse::<i32>().unwrap();
+
+            if one + other == target {
+                println!(
+                    "{} + {} = {} so the answer is {}",
+                    one,
+                    other,
+                    target,
+                    one * other
+                )
+            }
+
+            for pos3 in (0..input.len()).skip(pos2 + 1) {
+                let third = input[pos3].trim().parse::<i32>().unwrap();
+
+                if one + other + third == target {
+                    println!(
+                        "{} + {} + {} = {} so the answer is {}",
+                        one,
+                        other,
+                        third,
+                        target,
+                        one * other * third
+                    );
+                }
+            }
+        }
+    }
+}
