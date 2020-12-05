b9acf2297a15eed1f46847b944e862dd35e5c4fa
Alper Çugun <github@alper.nl>
2020-12-05 14:34:27 +0100

    Parsed everything finally?!?!?!?!

--- day-4/src/main.rs
+++ day-4/src/main.rs
@@ -6,12 +6,10 @@ use std::io;
 use std::iter::FromIterator;
 
 use nom::{
-    branch::alt,
-    branch::permutation,
+    branch::{alt, permutation},
     bytes::complete::{tag, take_while_m_n},
     character::complete::{digit1, multispace0},
     combinator::map_res,
-    dbg_dmp,
     sequence::{preceded, terminated},
     IResult,
 };
@@ -21,6 +19,12 @@ fn main() {
     let count = input.split("\n\n").filter(|b| check(b)).count();
 
     println!("Valid passports: {:?}", count);
+
+    let deep_count = input
+        .split("\n\n")
+        .filter(|b| !check_contents(b).is_err())
+        .count();
+    println!("Valid content passports: {}", deep_count);
 }
 
 fn check(block: &str) -> bool {
@@ -38,30 +42,32 @@ fn check(block: &str) -> bool {
     diff.copied().next().is_none()
 }
 
-fn check_contents(input: &str) -> IResult<&str, (&str, &str, u16, u16, u16)> {
+fn check_contents(input: &str) -> IResult<&str, (&str, &str, u16, u16, u16, &str, u16)> {
     permutation((
         check_eye_color,
         check_passport_id,
         check_birth_year,
         check_issue_year,
         check_expiry_year,
+        check_hair_color,
+        check_height,
     ))(input)
 }
 
 #[test]
 fn test_check_contents() {
     assert_eq!(
-        check_contents("pid:123456789 ecl:amb byr:1981 iyr:2020 eyr:2020"),
-        Ok(("", ("amb", "123456789", 1981, 2020, 2020)))
+        check_contents("hgt:182cm pid:123456789 ecl:amb hcl:#000000 byr:1981 iyr:2020 eyr:2020"),
+        Ok(("", ("amb", "123456789", 1981, 2020, 2020, "000000", 182)))
     );
 
     assert_eq!(
         check_contents(
             r"iyr:2011
         pid:123456789 ecl:amb
-        byr:1981 eyr:2025"
+        byr:1981 hgt:60in eyr:2025 hcl:#ffffff"
         ),
-        Ok(("", ("amb", "123456789", 1981, 2011, 2025)))
+        Ok(("", ("amb", "123456789", 1981, 2011, 2025, "ffffff", 60)))
     );
 
     assert_eq!(
@@ -155,4 +161,48 @@ fn test_check_passport_id() {
         Ok(("", "123456789"))
     );
     // assert_eq!(check_passport_id("pid:12345678912    "), Ok(("12", "12")));
+}
+
+fn check_hair_color(input: &str) -> IResult<&str, &str> {
+    let (input, _) = tag("hcl:#")(input)?;
+
+    terminated(take_while_m_n(6, 6, |c: char| c.is_digit(16)), multispace0)(input)
+}
+
+#[test]
+fn test_check_hair_color() {
+    assert_eq!(check_hair_color("hcl:#ffffff"), Ok(("", "ffffff")));
+    assert_eq!(check_hair_color("hcl:#000000  "), Ok(("", "000000")));
+
+    assert!(check_hair_color("hcl:#11111").is_err());
+}
+
+fn check_height(input: &str) -> IResult<&str, u16> {
+    let (input, _) = tag("hgt:")(input)?;
+
+    terminated(
+        alt((
+            terminated(
+                map_res(digit1, |s: &str| match s.parse::<u16>() {
+                    Ok(n) if n < 150 || n > 193 => {
+                        Err(io::Error::new(io::ErrorKind::Other, "out of range"))
+                    }
+                    Ok(n) => Ok(n),
+                    Err(e) => Err(io::Error::new(io::ErrorKind::Other, e.to_string())),
+                }),
+                tag("cm"),
+            ),
+            terminated(
+                map_res(digit1, |s: &str| match s.parse::<u16>() {
+                    Ok(n) if n < 59 || n > 76 => {
+                        Err(io::Error::new(io::ErrorKind::Other, "out of range"))
+                    }
+                    Ok(n) => Ok(n),
+                    Err(e) => Err(io::Error::new(io::ErrorKind::Other, e.to_string())),
+                }),
+                tag("in"),
+            ),
+        )),
+        multispace0,
+    )(input)
 }
