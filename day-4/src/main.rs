3f153a3f661a25ad785af1c7d7671b5e690715a8
Alper Çugun <github@alper.nl>
2020-12-05 20:51:36 +0100

    Got it base level working

--- day-4/src/main.rs
+++ day-4/src/main.rs
@@ -7,11 +7,13 @@ use std::iter::FromIterator;
 
 use nom::{
     branch::{alt, permutation},
-    bytes::complete::{tag, take_while_m_n},
+    bytes::complete::{tag, take_while, take_while_m_n},
     character::complete::{digit1, multispace0},
-    combinator::map_res,
+    character::is_alphanumeric,
+    combinator::{map_res, not, opt},
     sequence::{preceded, terminated},
     IResult,
+    dbg_dmp
 };
 
 fn main() {
@@ -20,11 +22,19 @@ fn main() {
 
     println!("Valid passports: {:?}", count);
 
-    let deep_count = input
-        .split("\n\n")
-        .filter(|b| !check_contents(b).is_err())
-        .count();
-    println!("Valid content passports: {}", deep_count);
+    for l in input.split("\n\n") {
+        println!("Input: {}", l);
+
+        let c = check_contents(l);
+
+        println!("Conents: {:?}", c);
+        println!("");
+    }
+    // let deep_count = input
+    //     .split("\n\n")
+    //     .filter(|b| !check_contents(b).is_err())
+    //     .count();
+    // println!("Valid content passports: {}", deep_count);
 }
 
 fn check(block: &str) -> bool {
@@ -42,7 +52,7 @@ fn check(block: &str) -> bool {
     diff.copied().next().is_none()
 }
 
-fn check_contents(input: &str) -> IResult<&str, (&str, &str, u16, u16, u16, &str, u16)> {
+fn check_contents(input: &str) -> IResult<&str, (&str, &str, u16, u16, u16, &str, u16, Option<&str>)> {
     permutation((
         check_eye_color,
         check_passport_id,
@@ -51,14 +61,15 @@ fn check_contents(input: &str) -> IResult<&str, (&str, &str, u16, u16, u16, &str
         check_expiry_year,
         check_hair_color,
         check_height,
+        check_cid,
     ))(input)
 }
 
 #[test]
 fn test_check_contents() {
     assert_eq!(
-        check_contents("hgt:182cm pid:123456789 ecl:amb hcl:#000000 byr:1981 iyr:2020 eyr:2020"),
-        Ok(("", ("amb", "123456789", 1981, 2020, 2020, "000000", 182)))
+        check_contents("hgt:182cm pid:123456789 ecl:amb hcl:#000000 byr:1981 iyr:2020 cid:12938981 eyr:2020"),
+        Ok(("", ("amb", "123456789", 1981, 2020, 2020, "000000", 182, Some("12938981"))))
     );
 
     assert_eq!(
@@ -67,7 +78,7 @@ fn test_check_contents() {
         pid:123456789 ecl:amb
         byr:1981 hgt:60in eyr:2025 hcl:#ffffff"
         ),
-        Ok(("", ("amb", "123456789", 1981, 2011, 2025, "ffffff", 60)))
+        Ok(("", ("amb", "123456789", 1981, 2011, 2025, "ffffff", 60, None)))
     );
 
     assert_eq!(
@@ -147,10 +158,27 @@ fn test_check_eye_color() {
     assert!(check_eye_color("ecl:bul").is_err());
 }
 
+fn check_cid(input: &str) -> IResult<&str, Option<&str>> {
+    opt(preceded(tag("cid:"), take_while(|c:char| c.is_alphanumeric())))(input)
+    // opt(terminated(preceded(tag("cid:"), take_while(|c| is_alphanumeric(c))), multispace0))(input)
+}
+
+#[test]
+fn test_check_cid() {
+    assert_eq!(check_cid("cid:123alper  lbabla"), Ok(("  lbabla", Some("123alper"))));
+    assert_eq!(check_cid("eid:123alper  lbabla"), Ok(("eid:123alper  lbabla", None)));
+    assert_eq!(check_cid("   cid:123alper  lbabla"), Ok(("   cid:123alper  lbabla", None)));
+    assert_eq!(check_cid("cid:alper"), Ok(("", Some("alper"))));
+    assert_eq!(check_cid("cid:123"), Ok(("", Some("123"))));
+}
+
 fn check_passport_id(input: &str) -> IResult<&str, &str> {
     let pid = take_while_m_n(9, 9, |c: char| c.is_digit(10));
 
-    terminated(preceded(tag("pid:"), pid), multispace0)(input)
+    terminated(
+        terminated(preceded(tag("pid:"), pid), not(digit1)),
+        multispace0,
+    )(input)
 }
 
 #[test]
@@ -160,7 +188,7 @@ fn test_check_passport_id() {
         check_passport_id("pid:123456789    "),
         Ok(("", "123456789"))
     );
-    // assert_eq!(check_passport_id("pid:12345678912    "), Ok(("12", "12")));
+    assert!(check_passport_id("pid:12345678912    ").is_err());
 }
 
 fn check_hair_color(input: &str) -> IResult<&str, &str> {
