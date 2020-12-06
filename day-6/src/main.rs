4143bc7320e069377d7a4f87a401d137bb092f27
Alper Çugun <github@alper.nl>
2020-12-06 12:12:23 +0100

    Got part 2 done

--- day-6/src/main.rs
+++ day-6/src/main.rs
@@ -6,6 +6,9 @@ fn main() {
 
     let first_result: u32 = input.split("\n\n").map(|group| unique_letter_count(group)).sum();
     println!("First result: {}", first_result);
+
+    let second_result: u32 = input.split("\n\n").map(|group| all_letter_count(group)).sum();
+    println!("Second result: {}", second_result);
 }
 
 fn unique_letter_count(group: &str) -> u32 {
@@ -18,4 +21,28 @@ fn unique_letter_count(group: &str) -> u32 {
     }
 
     letters.len() as u32
+}
+
+fn all_letter_count(group: &str) -> u32 {
+    let lines: Vec<&str> = group.lines().collect();
+    assert!(lines.len() > 0);
+
+    let first_line = lines[0];
+    let mut count = 0;
+
+    for c in first_line.chars() {
+        let mut missing = false;
+
+        for line in &lines { // We can iterate over all lines including the first one because by extension the first one too contains all the letters
+            if !line.contains(c) {
+                missing = true;
+            }
+        }
+
+        if !missing {
+            count += 1;
+        }
+    }
+
+    count
 }
