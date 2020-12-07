620c492e587c75d6645e3a8c14be6ae22bd0731f
Alper Çugun <github@alper.nl>
2020-12-07 23:15:33 +0100

    Got part 1 to work

--- day-7/src/main.rs
+++ day-7/src/main.rs
@@ -6,22 +6,24 @@ use std::collections::HashMap;
 use std::fs;
 
 use petgraph::prelude::*;
+use petgraph::algo::has_path_connecting;
+use petgraph::dot::Dot;
 
 fn main() {
     let input = fs::read_to_string("input.txt").expect("Dead file");
 
     let mut nodes: HashMap<String, NodeIndex> = HashMap::new();
-    let mut graph = Graph::<u32, u32>::new();
+    let mut graph = Graph::<String, u32>::new();
 
     for line in input.split('\n') {
-        println!("Record: {}", line);
+        println!("Input line: {}", line);
 
         let (container, colors) = parse_line(line);
 
         let node = match nodes.get(container.as_str()) {
             Some(n) => *n,
             None => {
-                let ni = graph.add_node(0);
+                let ni = graph.add_node(container.clone());
                 nodes.insert(container, ni);
                 ni
             }
@@ -31,7 +33,7 @@ fn main() {
             let contained_node = match nodes.get(col.as_str()) {
                 Some(n) => *n,
                 None => {
-                    let ni = graph.add_node(0);
+                    let ni = graph.add_node(col.clone());
                     nodes.insert(col, ni);
                     ni
                 }
@@ -41,7 +43,18 @@ fn main() {
         }
     }
 
-    println!("Generated graph: {:?}", graph);
+    println!("Generated graph: \n{:?}", Dot::new(&graph));
+
+    let target = nodes.get(&String::from("shiny gold")).unwrap();
+
+    let mut count = 0;
+    for (color, node) in nodes.iter() {
+        if (has_path_connecting(&graph, *node, *target, None)) {
+            count += 1;
+        }
+    }
+
+    println!("Colors that can contain shiny gold: {}", count-1);
 }
 
 fn parse_line(line: &str) -> (String, Vec<String>) {
@@ -59,14 +72,14 @@ fn parse_line(line: &str) -> (String, Vec<String>) {
                 contained_colors.push(cap[1].to_owned());
             }
 
-            let re_contained_several = Regex::new(r"(\d+) (.+?) bags").unwrap();
+            let re_contained_several = Regex::new(r"([2-9]|[1-9][0-9]) (.+?) bags").unwrap();
             for cap in re_contained_several.captures_iter(contained_bags.as_str()) {
                 contained_colors.push(cap[2].to_owned());
             }
         }
     }
 
-    println!("Color: {}, contains {:?}", color, contained_colors);
+    println!("Color: /{}/, contains {:?}", color, contained_colors);
 
     (color, contained_colors)
 }
