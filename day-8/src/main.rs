bc9d1061ff24ad085a1fc3e0d2febde9e73d63f3
Alper Çugun <github@alper.nl>
2020-12-09 00:21:05 +0100

    Part 2 done

--- day-8/src/main.rs
+++ day-8/src/main.rs
@@ -2,6 +2,11 @@ use std::collections::HashSet;
 use std::fs;
 
 fn main() {
+    part1();
+    part2();
+}
+
+fn part1() {
     let input = fs::read_to_string("input.txt").expect("Dead file");
 
     // let input = r"nop +0
@@ -22,11 +27,57 @@ fn main() {
 
     println!(
         "Accumulator after halt: {}",
-        run_program_until_loop(instructions).1
+        run_program_until_loop(&instructions).1
     );
 }
 
-#[derive(PartialEq, Debug)]
+fn part2() {
+    let input = fs::read_to_string("input.txt").expect("Dead file");
+
+    // let input = r"nop +0
+    // acc +1
+    // jmp +4
+    // acc +3
+    // jmp -3
+    // acc -99
+    // acc +1
+    // jmp -4
+    // acc +6";
+
+    let mut instructions: Vec<Instruction> = Vec::new();
+
+    for line in input.lines() {
+        instructions.push(parse_line(line));
+    }
+
+    for (pos, ins) in instructions.iter().enumerate() {
+        match ins {
+            Instruction::Jmp(n) => {
+                let mut alt_program = instructions.clone();
+                alt_program[pos] = Instruction::Nop(*n);
+
+                let (lp, acc) = run_program_until_loop(&alt_program);
+
+                if !lp {
+                    println!("Terminated without loop with accumulater: {} by changing the instruction {:?} at index: {}", acc, ins, pos);
+                }
+            }
+            Instruction::Nop(n) => {
+                let mut alt_program = instructions.clone();
+                alt_program[pos] = Instruction::Jmp(*n);
+
+                let (lp, acc) = run_program_until_loop(&alt_program);
+
+                if !lp {
+                    println!("Terminated without loop with accumulater: {} by changing the instruction {:?} at index: {}", acc, ins, pos);
+                }
+            }
+            Instruction::Acc(_n) => {}
+        }
+    }
+}
+
+#[derive(PartialEq, Debug, Clone)]
 enum Instruction {
     Nop(i32),
     Acc(i32),
@@ -35,7 +86,7 @@ enum Instruction {
 
 /// Run program
 /// Return true if it looped and was halted, false if it ran through
-fn run_program_until_loop(prog: Vec<Instruction>) -> (bool, i32) {
+fn run_program_until_loop(prog: &Vec<Instruction>) -> (bool, i32) {
     // Run program
     let mut accumulator = 0i32;
     let mut instruction_pointer = 0i32;
