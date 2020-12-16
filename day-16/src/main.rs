b2efd9decdbe074c060181eda7680120acc19df8
Alper Cugun-Gscheidel <alper.cugun-gscheidel@dcso.de>
2020-12-16 21:58:49 +0100

    Steady to the goal

--- day-16/src/main.rs
+++ day-16/src/main.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::collections::HashSet;
 use std::fs;
 
 fn main() {
@@ -82,13 +83,83 @@ fn main() {
 
     println!("Answer to part 1: {}", error_sum);
 
-    println!("Valid number of tickets: {}", valid_nearbys.len());
+    let my_ticket: Vec<u64> = vec![
+        149, 73, 71, 107, 113, 151, 223, 67, 163, 53, 173, 167, 109, 79, 191, 233, 83, 227, 229,
+        157,
+    ];
+    valid_nearbys.push(my_ticket.clone());
+
+    // println!("Valid number of tickets: {}", valid_nearbys.len());
 
     let ticket_fields: Vec<Vec<u64>> = (0..valid_nearbys[0].len())
         .map(|n| valid_nearbys.iter().map(move |v| v[n]).collect())
         .collect();
 
-    println!("Ticket fields: {:?}", ticket_fields);
+    // println!("Ticket fields: {:?}", ticket_fields);
+
+    let mut possible_rules: Vec<HashSet<&str>> = Vec::new();
+
+    for (i, field_vals) in ticket_fields.iter().enumerate() {
+        // println!("Checking {}th values: {:?}", i, field_vals);
+
+        let mut rules_for_field = HashSet::new();
+
+        for (fname, franges) in ranges.iter() {
+            if validate_values(&field_vals, *franges) {
+                // println!("+: {}", fname);
+                rules_for_field.insert(*fname);
+            } else {
+                // println!("X: {}", fname);
+            }
+        }
+
+        possible_rules.push(rules_for_field);
+    }
+
+    println!(
+        "possible rules: {:?}",
+        possible_rules
+            .iter()
+            .map(|r| r.len())
+            .collect::<Vec<usize>>()
+    );
+
+    let mut allocations: HashMap<usize, &str> = HashMap::new();
+    loop {
+        let single = possible_rules
+            .iter()
+            .enumerate()
+            .filter(|(i, f)| f.len() == 1)
+            .next()
+            .unwrap();
+        println!("Single: {:?}", single);
+
+        let field_name = single.1.iter().next().unwrap().clone();
+        allocations.insert(single.0, field_name);
+
+        // remove from all other possible
+        for i in 0..possible_rules.len() {
+            {
+                let s = &mut possible_rules[i];
+                s.remove(field_name);
+            }
+        }
+
+        if possible_rules.iter().all(|s| s.len() == 0) {
+            break;
+        }
+    }
+
+    println!("Allocations: {:?}", allocations);
+
+    println!(
+        "Answer 2? {}",
+        allocations
+            .iter()
+            .filter(|(i, s)| s.starts_with("departure"))
+            .map(|(i, s)| my_ticket[*i])
+            .product::<u64>()
+    );
 }
 
 fn parse_range(fragment: &str) -> (u64, u64) {
@@ -104,7 +175,9 @@ fn validate_values(values: &[u64], range: (u64, u64, u64, u64)) -> bool {
     let mut valid = true;
 
     for &value in values {
-        if !((value >= range.0 && value <= range.1) || (value >= range.2 && value <= range.3)) {}
+        if !((value >= range.0 && value <= range.1) || (value >= range.2 && value <= range.3)) {
+            valid = false;
+        }
     }
 
     valid
