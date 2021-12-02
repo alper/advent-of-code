f448a6452ff0a8234bca9807105a9d9245eca2f6
Alper Çugun <github@alper.nl>
2021-12-02 18:19:43 +0100

    Part 2 done

--- 2021/day2/src/main.rs
+++ 2021/day2/src/main.rs
@@ -27,4 +27,30 @@ fn main() {
     let down_delta = down_movements.iter().sum::<i32>();
 
     println!("Answer part 1: {:?}", forward_delta * down_delta);
+
+    let mut down: i32 = 0;
+    let mut aim: i32 = 0;
+    let mut forward: i32 = 0;
+
+    for mov in movements {
+        let dir = mov[0];
+        let amount = mov[1].parse::<i32>().unwrap();
+
+        match dir {
+            "down" => {
+                aim += amount;
+            },
+            "up" => {
+                aim -= amount;
+            },
+            "forward" => {
+                forward += amount;
+                down += aim * amount;
+            },
+            _ => {}
+        };
+    }
+
+    println!("Forward: {}, Down: {}", forward, down);
+    println!("Answer 2: {}", forward * down);
 }
