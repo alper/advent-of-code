067959fddf8ff6f256bcbd5b0b216dae15cdf7c6
Alper Çugun <github@alper.nl>
2021-12-04 12:09:32 +0100

    Tweak script a bit

--- 2021/create.fish
+++ 2021/create.fish
@@ -3,17 +3,22 @@
 set -l day $argv[1]
 set -l proj_name "day$day"
 
+if test -d $proj_name
+    exit 1
+end
+
 cargo new $proj_name
 
 cd $proj_name
 
 touch input.txt
+touch test_input.txt
 
 echo 'use std::fs;
 use itertools::{Itertools};
 
 fn main() {
-    let input = fs::read_to_string("input.txt").expect("File not readable");
+    let input = fs::read_to_string("test_input.txt").expect("File not readable");
 
     println!("Input: {:?}", input);
 }' > src/main.rs
