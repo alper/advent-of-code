72792798a8cd1101946459d47411145d708ed494
Alper Çugun <github@alper.nl>
2021-12-05 09:42:57 +0100

    Part 1 done!

---
+++ 2021/day5/src/main.rs
@@ -0,0 +1,55 @@
+use std::{fs, cmp};
+use itertools::{Itertools};
+use ndarray::{Array2, s};
+
+type Point = (usize, usize);
+
+fn main() {
+    let input = fs::read_to_string("input.txt").expect("File not readable");
+
+    let v: Vec<Vec<&str>> = input.lines().map(|l| l.split(" -> ").collect()).collect();
+
+    println!("Input: {:?}", v);
+
+    let mut a: Array2<usize> = Array2::zeros((1000, 1000));
+
+    // println!("{:?}", a);
+
+    // a[[1,2]] += 1;
+    // a[[10, 10]] += 1;
+
+
+
+    for l in &v {
+        let start: (usize, usize) = l[0].split(',').map(|d| d.parse::<usize>().unwrap()).collect_tuple().unwrap();
+        let end: (usize, usize) = l[1].split(',').map(|d| d.parse::<usize>().unwrap()).collect_tuple().unwrap();
+
+        println!("{:?} -> {:?}", start, end);
+
+        if start.0 == end.0 {
+            for i in cmp::min(start.1, end.1)..=cmp::max(start.1, end.1) {
+                a[[i, start.0]] += 1;
+            }
+        }
+
+        if start.1 == end.1 {
+            for j in cmp::min(start.0, end.0)..=cmp::max(start.0, end.0) {
+                a[[start.1, j]] += 1;
+            }
+        }
+    }
+
+    println!("{:?}", a);
+
+    a.mapv_inplace(|d| {
+        if d > 1 {
+            1
+        } else {
+            0
+        }
+    });
+
+    println!("Answer: {:?}", a.sum());
+
+
+}
