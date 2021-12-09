08fdf5110ce46200cb787e7cec9e515eb1ad59d2
Alper Çugun <github@alper.nl>
2021-12-09 22:24:52 +0100

    finally got it done

--- 2021/day8/src/main.rs
+++ 2021/day8/src/main.rs
@@ -19,90 +19,138 @@ fn main() {
 
     // Part 2
 
+    let pairs = input.lines().map(|l| (l.split('|').nth(0).unwrap().trim(), l.split('|').nth(1).unwrap().trim())).collect::<Vec<_>>();
 
+    let mut sum_of_digits = 0;
+
+    for (wiring, digits) in pairs {
+        let w = get_wiring(wiring);
+
+        let d: Vec<_> = digits.split(' ').map(|ds| decode_digit(ds, &w)).collect();
+
+        let res = 1000*d[0] + 100*d[1] + 10*d[2] + d[3];
+
+        sum_of_digits += res;
+
+        println!("{:?}", d);
+    }
+
+    println!("Answer 2: {}", sum_of_digits);
 
 }
 
 #[test]
 fn test_get_wiring() {
-    assert_eq!(get_wiring("acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab"), ['d', 'e', 'a', 'f', 'g', 'b', 'c']);
+    // assert_eq!(get_wiring("acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab"), ['d', 'e', 'a', 'f', 'g', 'b', 'c']);
 
+    println!("{:?}", get_wiring("acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab"));
 }
 
-fn get_wiring(groups: &str) -> [char; 7] {
+fn get_wiring(groups: &str) -> [HashSet<char>; 10] {
     let mut h = parse_frequencies(groups);
 
     let four_vec = h.remove(&4).unwrap();
     let four = four_vec.first().unwrap();
-    println!("Four: {:?}", four);
+    // println!("Four: {:?}", four);
 
     let one_vec = h.remove(&2).unwrap();
     let one = one_vec.first().unwrap();
-    println!("One: {:?}", one);
+    // println!("One: {:?}", one);
 
     let eight_vec = h.remove(&7).unwrap();
     let eight = eight_vec.first().unwrap();
-    println!("Eight: {:?}", eight);
+    // println!("Eight: {:?}", eight);
 
     let seven_vec = h.remove(&3).unwrap();
     let seven = seven_vec.first().unwrap();
-    println!("Seven: {:?}", seven);
+    // println!("Seven: {:?}", seven);
 
     // Get the six
-    let mut six_segs_3 = h.get(&6).unwrap();
-    let six_vec: Vec<_> = six_segs_3.iter().filter(|&s| s.intersection(&one).count() == 1).collect();
-    let six = six_vec.first().unwrap().clone();
+    let six_segs_3 = h.remove(&6).unwrap();
+    let six_vec: Vec<_> = six_segs_3.iter().cloned().filter(|s| s.intersection(&one).count() == 1).collect();
+    let six = six_vec.first().unwrap();
 
-    println!("Six: {:?}", six);
+    // println!("Six: {:?}", six);
 
     // Remove the six
-    let six_segs_2: Vec<_> = six_segs_3.iter().filter(|&s| s.intersection(&one).count() != 1).collect();
+    let six_segs_2: Vec<_> = six_segs_3.iter().cloned().filter(|s| s.intersection(&one).count() != 1).collect();
 
     // Get the three
-    let five_segs_3 = h.get(&5).unwrap();
-    let three_vec: Vec<_> = five_segs_3.iter().filter(|&s| s.is_superset(one)).collect();
-    let three = three_vec.first().unwrap().clone();
+    let five_segs_3 = h.remove(&5).unwrap();
+    let three_vec: Vec<_> = five_segs_3.iter().cloned().filter(|s| s.is_superset(&one)).collect();
+    let three = three_vec.first().unwrap();
 
-    println!("Three: {:?}", three);
+    // println!("Three: {:?}", three);
 
     // Remove the three
-    let five_segs_2: Vec<_> = five_segs_3.iter().filter(|&s| s != three).collect();
+    let five_segs_2: Vec<_> = five_segs_3.iter().cloned().filter(|s| s != three).collect();
 
-    println!("Five_2: {:?}", five_segs_2);
+    // println!("Five_2: {:?}", five_segs_2);
 
     // Get the five
-    let five_vec: Vec<_> = five_segs_2.iter().filter(|&s| six.is_superset(s)).collect();
-    let five = five_vec.first().unwrap().clone();
+    let five_vec: Vec<_> = five_segs_2.iter().cloned().filter(|s| six.is_superset(s)).collect();
+    let five = five_vec.first().unwrap();
 
-    println!("Five: {:?}", five);
+    // println!("Five: {:?}", five);
 
     // Get the two
-    let two_vec: Vec<_> = five_segs_2.iter().filter(|&s| s != five).collect();
-    let two = two_vec.first().unwrap().clone();
-    println!("Two: {:?}", two);
+    let two_vec: Vec<_> = five_segs_2.iter().cloned().filter(|s| s != five).collect();
+    let two = two_vec.first().unwrap();
+    // println!("Two: {:?}", two);
 
     // Get the nine
-    let nine_vec: Vec<_> = six_segs_2.iter().filter(|&s| s.is_superset(four)).collect();
-    let nine = nine_vec.first().unwrap().clone();
+    let nine_vec: Vec<_> = six_segs_2.iter().cloned().filter(|s| s.is_superset(&four)).collect();
+    let nine = nine_vec.first().unwrap();
 
-    println!("Nine: {:?}", nine);
+    // println!("Nine: {:?}", nine);
 
     let zero_vec: Vec<_> = six_segs_2.iter().filter(|&s| s != nine).collect();
-    let zero = zero_vec.first().unwrap().clone();
+    let zero = *zero_vec.first().unwrap();
 
-    println!("Zero: {:?}", zero);
+    // println!("Zero: {:?}", zero);
 
     let d = eight.difference(zero).next().unwrap();
     let c = eight.difference(six).next().unwrap();
     let e = eight.difference(nine).next().unwrap();
-    let b = nine.difference(three).next().unwrap();
-    let a = seven.difference(one).next().unwrap();
-    let g = zero.difference(seven).filter(|&w| w != b && w != e).next().unwrap();
+    let b = nine.difference(&three).next().unwrap();
+    let a = seven.difference(&one).next().unwrap();
+    let g = zero.difference(&seven).filter(|&w| w != b && w != e).next().unwrap();
     let f = one.iter().filter(|&w| w != c).next().unwrap();
 
     println!("{} {} {} {} {} {} {}", a, b, c, d, e, f, g);
 
-    return [*a, *b, *c, *d, *e, *f, *g];
+    return [zero.clone(),
+        one.clone(),
+        two.clone(),
+        three.clone(),
+        four.clone(),
+        five.clone(),
+        six.clone(),
+        seven.clone(),
+        eight.clone(),
+        nine.clone()];
+}
+
+#[test]
+fn test_decode_digit() {
+    let w = get_wiring("acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab");
+
+    assert_eq!(decode_digit("cdfeb", &w), 5);
+    assert_eq!(decode_digit("fcadb", &w), 3);
+    assert_eq!(decode_digit("cdfeb", &w), 5);
+    assert_eq!(decode_digit("cdbaf", &w), 3);
+}
+
+fn decode_digit(garbled: &str, wiring: &[HashSet<char>; 10]) -> usize {
+    let s: HashSet<char> = garbled.chars().collect();
+
+    for i in 0..10 {
+        if wiring[i] == s {
+            return i;
+        }
+    }
+
+    return 10;
 }
 
 #[test]
