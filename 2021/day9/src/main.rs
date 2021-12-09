bce2fc3546b2d2cc28752ccf22a04671dda9148c
Alper Çugun <github@alper.nl>
2021-12-09 17:58:38 +0100

    made recursive function work

--- 2021/day9/src/main.rs
+++ 2021/day9/src/main.rs
@@ -77,6 +77,54 @@ fn main() {
 
 }
 
-fn basin_size(point: (usize, usize), arr: Array2D) {
+fn basin_size(point: (usize, usize), arr: &mut Array2D<isize>) -> usize {
+
+
+    1
+}
+
+#[test]
+fn test_basin_size_rec() {
+    let rows: Vec<Vec<isize>> = vec![
+        vec![1, 9, 3],
+        vec![4, 9, 6]
+        ];
+    let mut array = Array2D::from_rows(&rows);
+
+    println!("Basin size: {}", basin_size_rec((0, 0), &mut array));
+
+    assert_eq!(1, 2);
+}
+
+fn basin_size_rec(point: (usize, usize), arr: &mut Array2D<isize>) -> usize {
+    println!("Got arr: {:?}", arr);
+    println!("Got point: {:?}", point);
+
+    if let Some(p) = arr.get_mut(point.0, point.1) {
+        if *p == 9 || *p == -1 {
+            return 0;
+        } else {
+            arr.set(point.0, point.1, -1);
+
+            let mut count = 1;
+
+            if point.0 > 0 {
+                count += basin_size_rec((point.0-1, point.1), arr);
+            }
+
+            count += basin_size_rec((point.0+1, point.1), arr);
+
+            if point.1 > 0 {
+                count += basin_size_rec((point.0, point.1-1), arr);
+
+            }
+
+            count += basin_size_rec((point.0, point.1+1), arr);
+
+            return count;
+        }
+    } else {
+        return 0;
+    }
 
 }
