c293bf8b14cd485b3107483bbbc3bbebaaf3e436
Alper Çugun <github@alper.nl>
2021-12-11 09:59:27 +0100

    Part 1 done

---
+++ 2021/day11/src/main.rs
@@ -0,0 +1,159 @@
+use std::fs;
+use itertools::{Itertools};
+use grid::*;
+use std::fmt;
+
+struct Octopus {
+    energy_level: usize,
+    flashed: bool,
+}
+
+impl Default for Octopus {
+    fn default() -> Self {
+        Octopus {
+            energy_level: 0,
+            flashed: false,
+        }
+    }
+}
+
+impl fmt::Debug for Octopus {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.energy_level)
+
+        // if !self.marked {
+        //     write!(f, "{}", self.board_value)
+        // } else {
+        //     write!(f, "*{}*", self.board_value)
+        // }
+    }
+}
+
+type OctopusGrid = Grid<Octopus>;
+
+fn main() {
+    let input = fs::read_to_string("input.txt").expect("File not readable");
+
+    let a: Vec<_> = input
+        .replace("\n", "")
+        .chars()
+        .map(|c| c.to_digit(10).unwrap() as usize)
+        .map(|d| Octopus{ energy_level: d, flashed: false})
+        .collect();
+
+    let mut g: OctopusGrid = Grid::from_vec(a, 10);
+
+    println!("Input: {:?}", g);
+
+    let mut flashes = 0;
+    for _ in 0..100 {
+        flashes += step_octopuses(&mut g);
+    }
+
+    println!("Flashes: {:?}", flashes);
+}
+
+fn step_octopuses(g: &mut OctopusGrid) -> usize {
+    // Increase each by 1
+    for o in g.iter_mut() {
+        o.energy_level +=1 ;
+    }
+
+    let mut total_flash_count = 0;
+
+    // Process flashes
+    loop {
+        println!("Loop");
+
+        let mut local_flash_count = 0;
+
+        for i in 0..g.rows() {
+            for j in 0..g.cols() {
+                let mut o = g.get_mut(i, j).unwrap();
+
+                // println!("Octo energ: {}", o.energy_level);
+
+                if o.energy_level > 9 && !o.flashed {
+                    // Flash
+                    o.flashed = true;
+
+                    // Increment the neighbors
+                    let neighbors = get_neighbors(i, j, g.rows(), g.cols());
+                    for n in neighbors {
+                        // println!("N: {:?}", n);
+                        let mut octo_neighbor = g.get_mut(n.0, n.1).unwrap();
+                        octo_neighbor.energy_level += 1;
+                    }
+
+                    local_flash_count += 1;
+                }
+            }
+        }
+
+        println!("Flash count: {}", local_flash_count);
+        total_flash_count += local_flash_count;
+        if local_flash_count == 0 {
+            break;
+        }
+    }
+
+    // Set all flashed octopuses back
+    for o in g.iter_mut() {
+        if o.flashed {
+            o.energy_level = 0;
+            o.flashed = false;
+        }
+    }
+
+    total_flash_count
+}
+
+#[test]
+fn test_get_neighbors() {
+    // 0 1 2
+    // 3 4 5
+    // 6 7 8
+
+    assert_eq!(get_neighbors(0, 0, 3, 3), vec![(0, 1), (1, 0), (1, 1)]);
+    assert_eq!(get_neighbors(1, 0, 3, 3), vec![(0, 0), (0, 1), (1, 1), (2, 0), (2, 1)]);
+    assert_eq!(get_neighbors(2, 2, 3, 3), vec![(1, 1), (1, 2), (2, 1)]);
+    assert_eq!(get_neighbors(1, 1, 3, 3), vec![(0, 0), (0, 1), (0, 2), (1, 0), (1, 2), (2, 0), (2, 1), (2, 2)]);
+}
+
+fn get_neighbors(r: usize, c: usize, rows: usize, cols: usize) -> Vec<(usize, usize)> {
+    let mut v = vec![];
+
+    if r > 0 && c > 0 { // NW
+        v.push((r-1, c-1));
+    }
+
+    if r > 0 { // N
+        v.push((r-1, c));
+    }
+
+    if r > 0 && c < cols-1 { // NE
+        v.push((r-1, c+1));
+    }
+
+    if c > 0 { // E
+        v.push((r, c-1));
+    }
+
+    if c < cols-1 { // W
+        v.push((r, c+1));
+    }
+
+    if r < rows-1 && c > 0 { // SW
+        v.push((r+1, c-1));
+    }
+
+    if r < rows-1 { // S
+        v.push((r+1, c));
+    }
+
+    if r < rows-1 && c < cols-1 { // SE
+        v.push((r+1, c+1));
+    }
+
+    v
+}
