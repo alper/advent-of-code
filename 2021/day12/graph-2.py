ac4aa51676a06faeaec8f89ae98ff51be5c9863b
Alper Çugun <github@alper.nl>
2021-12-12 18:30:27 +0100

    Cribbed solution for part 2

--- 2021/day12/graph-2.py
+++ 2021/day12/graph-2.py
@@ -12,36 +12,29 @@ class Graph:
         self.graph[parts[0]].append(parts[1])
         self.graph[parts[1]].append(parts[0])
 
-    def all_paths_rec(self, u, d, visited, path):
-        if u.islower():
-            visited[u] = True
+    def all_paths_rec(self, u, visited, path, twice=False):
+        if u == 'end':
+            self.paths.append(path + ['end'])
+            return
 
-        path.append(u)
+        if u.islower() and u in visited:
+            if twice or u == 'start' or u == 'end':
+                return
+            else:
+                twice = True
 
-        if u == d:
-            self.paths.append(path)
-        else:
-            for i in self.graph[u]:
-                if visited[i] == False:
-                    self.all_paths_rec(i, d, visited, path)
-
-        path.pop()
-        visited[u] = False
-
-    def all_paths(self, s, e):
-
-        visited = defaultdict(lambda: False)
-        path = []
-
-        self.all_paths_rec(s, e, visited, path)
+        for e in self.graph[u]:
+            self.all_paths_rec(e, path + [u], visited + [u], twice)
 
 g = Graph()
 
-with open('test_input.txt') as f:
+with open('input.txt') as f:
     for line in f.readlines():
-        print(line)
+        # print(line)
         g.add_edge(line.strip())
 
-g.all_paths("start", "end")
 
+g.all_paths_rec('start', [], [], False)
+
+print(g.paths)
 print(len(g.paths))
