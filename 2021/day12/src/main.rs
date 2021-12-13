eece16923afefc5c49d0a769d17a0bdbf1bfad56
Alper Çugun <github@alper.nl>
2021-12-13 15:19:29 +0100

    Made it work

--- 2021/day12/src/main.rs
+++ 2021/day12/src/main.rs
@@ -1,101 +1,61 @@
-use itertools::Itertools;
-use petgraph::algo;
-use petgraph::graph::Node;
-use petgraph::prelude::*;
 use std::collections::HashMap;
 use std::fs;
 
-// type Graph<'a> = HashMap<&'a str, Vec<&'a str>>;
+type Graph<'a> = HashMap<&'a str, Vec<&'a str>>;
 
 fn main() {
-    let input = fs::read_to_string("test_input.txt").expect("File not readable");
+    let input = fs::read_to_string("input.txt").expect("File not readable");
 
     let graph = parse_graph(&input);
 
-    // let mut paths: Vec<Vec<&str>> = Vec::new();
-    // depth_first(vec!["A"], vec![], &mut paths, &graph);
+    // println!("Graph: {:?}", graph);
 
-    println!("Input: {:?}", graph);
-
-    let start = graph.node_indices().find(|i| graph[*i] == "start").unwrap();
-    let end = graph.node_indices().find(|i| graph[*i] == "end").unwrap();
-
-    println!("Start: {:?} end: {:?}", start, end);
-
-    let ways = algo::all_simple_paths::<Vec<_>, _>(&graph, start, end, 0, None).collect::<Vec<_>>();
-
-    println!("Ways: {:?}", ways.len());
+    let mut path: Vec<String> = Vec::new();
+    let mut visited: Vec<String> = Vec::new();
+    paths(&graph, "start", &mut path, &mut visited, false);
 }
 
-fn parse_graph(inp: &str) -> UnGraph<&str, ()> {
+fn parse_graph(inp: &str) -> Graph {
     let parsed: Vec<(_, _)> = inp
         .lines()
         .map(|l| (l.split('-').next().unwrap(), l.split('-').nth(1).unwrap()))
         .collect();
 
-    // let mut graph: Graph = HashMap::new();
+    let mut graph: Graph = HashMap::new();
 
-    // for el in parsed {
-    //     let dests = graph.entry(el.0).or_insert(vec![]);
-    //     dests.push(el.1);
-    // }
+    for el in parsed {
+        let dests_lr = graph.entry(el.0.clone()).or_insert(vec![]);
+        dests_lr.push(el.1.clone());
 
-    // graph
-
-    let mut g = UnGraph::<&str, ()>::default();
-
-    let mut node_map: HashMap<&str, NodeIndex> = HashMap::new();
-
-    // // Nodes
-    // let nodes: Vec<&str> = parsed.iter().map(|(f, t)| *f).unique().collect();
-    // for node in nodes {
-    //     if !node_map.contains_key(node)
-    //     let ni = g.add_node(node);
-
-    //     node_map.insert(node, ni.index());
-    // }
-
-    for edge in parsed {
-        if !node_map.contains_key(edge.0) {
-            let ni = g.add_node(edge.0);
-            node_map.insert(edge.0, ni);
-        }
-
-        if !node_map.contains_key(edge.1) {
-            let ni = g.add_node(edge.1);
-            node_map.insert(edge.1, ni);
-        }
-
-        let from_index = *node_map.get(edge.0).unwrap();
-        let to_index = *node_map.get(edge.1).unwrap();
-
-        g.add_edge(from_index, to_index, ());
+        let dests_rl = graph.entry(el.1.clone()).or_insert(vec![]);
+        dests_rl.push(el.0.clone());
     }
 
-    g
+    graph
 }
 
-// fn depth_first(path: Vec<&str>, visited: Vec<&str>, paths: &mut Vec<Vec<&str>>, graph: &Graph) {
-//     // Do logic
-//     let last_node = *path.last().unwrap_or(&&"");
+fn paths(g: &Graph, v: &str, path: &mut Vec<String>, visited: &mut Vec<String>, mut twice: bool) {
+    if v == "end" {
+        path.push("end".to_string());
+        println!("{:?}", path);
+        return;
+    }
 
-//     if last_node != "end" {
-//         let dests = graph.get(last_node).unwrap();
+    if v.chars().all(|c| c.is_lowercase()) && visited.contains(&v.to_owned()) {
+        if twice || v == "start" || v == "end" {
+            return;
+        } else {
+            twice = true;
+        }
+    }
 
-//         for dest in dests {
-//             if !visited.contains(dest) {
-//                 let new_path = path.clone();
-//                 new_path.push(dest);
+    for e in &g[v] {
+        let mut pat = path.clone();
+        pat.push(v.to_owned());
 
-//                 let new_visited = visited.clone();
-//                 if !dest.chars().all(|c| c.is_uppercase()) {
-//                     new_visited.push(dest);
-//                 }
+        let mut vis = visited.clone();
+        vis.push(v.to_owned());
 
-//                 depth_first(new_path, new_visited, paths, graph)
-//             }
-//         }
-//     } else {
-//         paths.push(path);
-//     }
-// }
+        paths(g, *e, &mut pat, &mut vis, twice);
+    }
+}
