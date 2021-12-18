f89036e574ae4ae5513b0dc7432cecfab1dc8a4c
Alper Çugun <github@alper.nl>
2021-12-18 01:26:08 +0100

    Got a long way with the parser

--- 2021/day16/src/main.rs
+++ 2021/day16/src/main.rs
@@ -1,6 +1,7 @@
 extern crate nom;
 
 use itertools::Itertools;
+use nom::branch::alt;
 use nom::bytes::complete::tag;
 use nom::character::complete::one_of;
 use nom::combinator::map;
@@ -18,46 +19,171 @@ fn main() {
 }
 
 #[derive(Debug)]
-enum PacketType {
-    Operator,
-    Literal(usize),
+enum Packet {
+    Operator {
+        version: usize,
+        subpackets: Vec<Packet>,
+    },
+    Literal {
+        version: usize,
+        value: usize,
+    },
 }
 
 #[test]
 fn test_parse() {
     println!("{:?}", literal_packet("110100101111111000101000"));
 
+    println!(
+        "8A004A801A8002F478 {:?}",
+        parse_packet(&hex_to_bin("8A004A801A8002F478"))
+    );
+
+    // println!(
+    //     "620080001611562C8802118E34 {:?}",
+    //     parse_packet(&hex_to_bin("620080001611562C8802118E34"))
+    // );
+
+    // println!(
+    //     "C0015000016115A2E0802F182340 {:?}",
+    //     parse_packet(&hex_to_bin("C0015000016115A2E0802F182340"))
+    // );
+
+    println!(
+        "A0016C880162017C3686B18A3D4780 {:?}",
+        parse_packet(&hex_to_bin("A0016C880162017C3686B18A3D4780"))
+    );
+
+    println!(
+        "38006F45291200 {:?}",
+        parse_packet(&hex_to_bin("38006F45291200"))
+    );
+
+    println!(
+        "EE00D40C823060 {:?}",
+        parse_packet(&hex_to_bin("EE00D40C823060"))
+    );
+
     assert_eq!(1, 2);
 }
 
-fn literal_packet(input: &str) -> IResult<&str, PacketType> {
-    let (input, packet_version) =
-        map(count(one_of("01"), 3), |v| v.iter().collect::<String>())(input)?;
-    println!("Packet version: {:?}", packet_version);
+fn parse_packet(input: &str) -> IResult<&str, Packet> {
+    alt((literal_packet, operator_packet))(input)
+}
 
+fn literal_packet(input: &str) -> IResult<&str, Packet> {
+    let (input, packet_version) = packet_version(input)?;
     let (input, packet_type) = tag("100")(input)?;
-
     let (input, number_blocks) = many0(preceded(tag("1"), four_digits))(input)?;
-    println!("Number blocks: {:?}", number_blocks);
-
     let (input, number_end_block) = preceded(tag("0"), four_digits)(input)?;
-    println!("Number end block: {:?}", number_end_block);
 
     let mut v = Vec::from(number_blocks);
     v.push(number_end_block);
     let n = usize::from_str_radix(&v.join(""), 2).unwrap();
 
-    Ok((input, PacketType::Literal(n)))
+    Ok((
+        input,
+        Packet::Literal {
+            version: packet_version,
+            value: n,
+        },
+    ))
+}
+
+fn packet_version(input: &str) -> IResult<&str, usize> {
+    map(count(one_of("01"), 3), |v| {
+        usize::from_str_radix(&v.iter().collect::<String>(), 2).unwrap()
+    })(input)
+}
+
+#[derive(Debug)]
+enum PacketType {
+    Literal,
+    Operator,
+}
+
+fn packet_type(input: &str) -> IResult<&str, PacketType> {
+    let (input, t) = count(one_of("01"), 3)(input)?;
+
+    Ok((
+        input,
+        if t.iter().join("") == "100" {
+            PacketType::Literal
+        } else {
+            PacketType::Operator
+        },
+    ))
+}
+
+enum LengthTypeID {
+    TotalLength(usize),
+    NumberOfSubpackets(usize),
+}
+
+fn packet_length_type_id(input: &str) -> IResult<&str, LengthTypeID> {
+    println!("Packet length type input: {}", input);
+
+    let (input, c) = one_of("01")(input)?;
+
+    if c == '0' {
+        let (input, n) = map(count(one_of("01"), 15), |v| {
+            usize::from_str_radix(&v.iter().collect::<String>(), 2).unwrap()
+        })(input)?;
+
+        println!("Parsed length: {}", n);
+
+        return Ok((input, LengthTypeID::TotalLength(n)));
+    } else {
+        let (input, n) = map(count(one_of("01"), 11), |v| {
+            usize::from_str_radix(&v.iter().collect::<String>(), 2).unwrap()
+        })(input)?;
+
+        println!("Parsed subpackets: {}", n);
+
+        return Ok((input, LengthTypeID::NumberOfSubpackets(n)));
+    }
+}
+
+fn operator_packet(input: &str) -> IResult<&str, Packet> {
+    println!("Parse operator: {}", input);
+
+    let (input, packet_version) = packet_version(input)?;
+    let (input, p_type) = packet_type(input)?;
+    let (input, p_length) = packet_length_type_id(input)?;
+
+    if let LengthTypeID::TotalLength(n) = p_length {
+        let (ip, subpackets) = many0(parse_packet)(&input[0..n])?;
+
+        return Ok((
+            &input[n + 1..],
+            Packet::Operator {
+                version: packet_version,
+                subpackets: subpackets,
+            },
+        ));
+    } else if let LengthTypeID::NumberOfSubpackets(n) = p_length {
+        let (input, subpackets) = count(parse_packet, n)(input)?;
+
+        return Ok((
+            input,
+            Packet::Operator {
+                version: packet_version,
+                subpackets: subpackets,
+            },
+        ));
+    } else {
+        return Ok((
+            input,
+            Packet::Operator {
+                version: 0,
+                subpackets: vec![],
+            },
+        ));
+    }
 }
 
 fn four_digits(input: &str) -> IResult<&str, String> {
     map(count(one_of("01"), 4), |v| v.iter().collect::<String>())(input)
-}
-
-fn operator_packet(input: &str) -> IResult<&str, PacketType> {
-    let (input, packet_version) = count(one_of("01"), 3)(input)?;
-
-    Ok((input, PacketType::Operator))
 }
 
 #[test]
