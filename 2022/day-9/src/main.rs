46d015577732bcd9c4556f51dd2a4e362af43ead
Alper Çugun <github@alper.nl>
2022-12-09 13:44:51 +0100

    Finished part 2

--- 2022/day-9/src/main.rs
+++ 2022/day-9/src/main.rs
@@ -8,6 +8,12 @@ struct Pos {
     y: isize,
 }
 
+impl Default for Pos {
+    fn default() -> Self {
+        Self { x: 0, y: 0 }
+    }
+}
+
 // Grid to the top right
 fn head_move(pos: Pos, step: Step) -> Pos {
     match step {
@@ -121,6 +127,35 @@ fn tail_move(head_pos: Pos, tail_pos: Pos) -> Pos {
         };
     }
 
+    // Diagonal two
+    if head_pos.x - tail_pos.x == 2 && head_pos.y - tail_pos.y == 2 {
+        return Pos {
+            x: tail_pos.x + 1,
+            y: tail_pos.y + 1,
+        };
+    }
+
+    if head_pos.x - tail_pos.x == 2 && head_pos.y - tail_pos.y == -2 {
+        return Pos {
+            x: tail_pos.x + 1,
+            y: tail_pos.y - 1,
+        };
+    }
+
+    if head_pos.x - tail_pos.x == -2 && head_pos.y - tail_pos.y == 2 {
+        return Pos {
+            x: tail_pos.x - 1,
+            y: tail_pos.y + 1,
+        };
+    }
+
+    if head_pos.x - tail_pos.x == -2 && head_pos.y - tail_pos.y == -2 {
+        return Pos {
+            x: tail_pos.x - 1,
+            y: tail_pos.y - 1,
+        };
+    }
+
     // Coinciding or diagonal touching
     return tail_pos;
 }
@@ -134,16 +169,23 @@ enum Step {
 }
 
 fn do_step(head: Pos, tail: Pos, step: Step) -> (Pos, Pos) {
-    println!("Do step: {:?}", step);
+    // println!("Do step: {:?}", step);
 
     let new_head = head_move(head, step);
     let new_tail = tail_move(new_head, tail);
 
-    println!("New Head: {:?}, New Tail: {:?}", new_head, new_tail);
+    // println!("New Head: {:?}, New Tail: {:?}", new_head, new_tail);
     // println!("Head: {:?}", new_head);
     // println!("Tail: {:?}", new_tail);
 
     (new_head, new_tail)
+}
+
+fn render_snake(snake: [Pos; 10]) {
+    for i in 0..snake.len() {
+        print!("{}x{} ", snake[i].x, snake[i].y);
+    }
+    println!("");
 }
 
 fn main() {
@@ -154,8 +196,8 @@ fn main() {
 
     let mut visited: HashSet<Pos> = HashSet::new();
 
-    let mut head = Pos { x: 0, y: 0 };
-    let mut tail = Pos { x: 0, y: 0 };
+    let mut head = Default::default();
+    let mut tail = Default::default();
 
     visited.insert(tail);
 
@@ -184,5 +226,45 @@ fn main() {
     // Part 2
     println!("Part 2");
 
-    println!("Answer part 2: {:?}", "");
+    let mut visited: HashSet<Pos> = HashSet::new();
+
+    let mut snake: [Pos; 10] = Default::default();
+
+    visited.insert(snake[9]);
+
+    for l in input.lines() {
+        let (dir, amount) = l.split(' ').collect_tuple().unwrap();
+
+        let step = match dir {
+            "U" => Step::Up,
+            "D" => Step::Down,
+            "L" => Step::Left,
+            "R" => Step::Right,
+            _ => unimplemented!("Should not happen!"),
+        };
+
+        let amount = str::parse::<isize>(amount).unwrap();
+
+        println!("Move: {amount} in {:?}", step);
+
+        for _ in 0..amount {
+            snake[0] = head_move(snake[0], step);
+            snake[1] = tail_move(snake[0], snake[1]);
+            snake[2] = tail_move(snake[1], snake[2]);
+            snake[3] = tail_move(snake[2], snake[3]);
+            snake[4] = tail_move(snake[3], snake[4]);
+            snake[5] = tail_move(snake[4], snake[5]);
+            snake[6] = tail_move(snake[5], snake[6]);
+            snake[7] = tail_move(snake[6], snake[7]);
+            snake[8] = tail_move(snake[7], snake[8]);
+            snake[9] = tail_move(snake[8], snake[9]);
+
+            render_snake(snake);
+
+            visited.insert(snake[9]);
+        }
+        println!("");
+    }
+
+    println!("Answer part 2: {:?}", visited.len());
 }
