42de66f1d542ab9dcfba3f551c22480a3a5f27bb
Alper Çugun <github@alper.nl>
2022-12-14 19:38:47 +0100

    Make variable names more descriptive

--- 2015/aoc-cookiecutter/{{ cookiecutter.directory_name }}/aoc.py
+++ 2015/aoc-cookiecutter/{{ cookiecutter.directory_name }}/aoc.py
@@ -8,24 +8,24 @@ def get_input():
     return open("full_input.txt").read()
 
 
-def calculate_part1(i):
+def calculate_part1(puzzle_input):
     print("Calculating for", i)
 
     return 1
 
 
-def calculate_part2(i):
+def calculate_part2(puzzle_input):
     print("Calculating", i)
 
     return 1
 
 
-def part1(i):
-    return calculate_part1(i)
+def part1(puzzle_input):
+    return calculate_part1(puzzle_input)
 
 
-def part2(i):
-    return calculate_part2(i)
+def part2(puzzle_input):
+    return calculate_part2(puzzle_input)
 
 
 def test_calculation():
@@ -35,7 +35,7 @@ def test_calculation():
 
 
 if __name__ == "__main__":
-    i = get_input()
+    puzzle_input = get_input()
 
-    # print("Part 1 answer:", part1(i))
-    # print("Part 2 answer:", part2(i))
+    # print("Part 1 answer:", part1(puzzle_input))
+    # print("Part 2 answer:", part2(puzzle_input))
