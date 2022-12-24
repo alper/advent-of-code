583fc05efb7929fb7b5faf61c226052bec70a466
Alper Çugun <github@alper.nl>
2022-12-24 22:09:47 +0100

    Got it to work on the test input

--- 2022/day-24/aoc.py
+++ 2022/day-24/aoc.py
@@ -4,8 +4,11 @@ import collections
 from itertools import chain
 from typing import List, Tuple
 
-COLS = 6
-ROWS = 4
+# COLS = 6
+# ROWS = 4
+
+COLS = 120
+ROWS = 25
 
 CoordList = List[Tuple[int, int]]
 
@@ -49,6 +52,27 @@ def step_all(
     )
 
 
+def get_next_positions(pos: Tuple[int, int]) -> List[Tuple[int, int]]:
+    n = []
+
+    # Staying put is also a potential position
+    n.append((pos[0], pos[1]))
+
+    if pos[0] > 0:
+        n.append((pos[0] - 1, pos[1]))
+
+    if pos[0] < COLS - 1:
+        n.append((pos[0] + 1, pos[1]))
+
+    if pos[1] > 0:
+        n.append((pos[0], pos[1] - 1))
+
+    if pos[1] < ROWS - 1:
+        n.append((pos[0], pos[1] + 1))
+
+    return n
+
+
 def print_map(
     northers: CoordList, easters: CoordList, southers: CoordList, westers: CoordList
 ):
@@ -68,7 +92,8 @@ def print_map(
 
 
 def get_input() -> Tuple[CoordList, CoordList, CoordList, CoordList]:
-    i = open("test_input.txt")
+    # i = open("test_input.txt")
+    i = open("full_input.txt")
 
     northers, easters, southers, westers = [], [], [], []
     for j, l in enumerate(list(i)[1:-1]):
@@ -96,22 +121,64 @@ def calculate_part1(puzzle_input):
     print("Start")
     print_map(northers, easters, southers, westers)
 
-    northers, easters, southers, westers = step_all(
-        northers, easters, southers, westers
-    )
+    blizzards = step_all(northers, easters, southers, westers)
 
-    print()
-    print("Round 1")
-    print_map(northers, easters, southers, westers)
+    pos = (0, 0)
 
-    northers, easters, southers, westers = step_all(
-        northers, easters, southers, westers
-    )
+    minutes = 1
 
-    print()
-    print("Round 2")
-    print_map(northers, easters, southers, westers)
+    candidate_state = collections.deque()
+    seen = set()
 
+    candidate_state.append((pos, minutes, blizzards))
+
+    while True:
+        pos, minutes, blizzards = candidate_state.popleft()
+        print(f"In minute: {minutes}, position: {pos} seen size: {len(seen)}")
+        print_map(blizzards[0], blizzards[1], blizzards[2], blizzards[3])
+        print()
+
+        if (pos, map(lambda b: frozenset(b), blizzards)) in seen:
+            continue
+        else:
+            seen.add((pos, map(lambda b: frozenset(b), blizzards)))
+
+        if pos == (COLS - 1, ROWS - 1):
+            print("Done at minute:", minutes + 1)
+            break
+
+        # Step all the blizzards
+        blizzards = step_all(blizzards[0], blizzards[1], blizzards[2], blizzards[3])
+
+        # Generate all the next positions based on the current pos
+        next_positions = get_next_positions(pos)
+
+        # Check which of the next positions are valid next positions
+        # and add them to a queue
+        for next_pos in next_positions:
+            if (
+                next_pos in blizzards[0]
+                or next_pos in blizzards[1]
+                or next_pos in blizzards[2]
+                or next_pos in blizzards[3]
+            ):
+                pass
+            else:
+                candidate_state.append((next_pos, minutes + 1, blizzards))
+
+    # print()
+    # print("Round 1")
+    # print_map(northers, easters, southers, westers)
+
+    # northers, easters, southers, westers = step_all(
+    #     northers, easters, southers, westers
+    # )
+
+    # print()
+    # print("Round 2")
+    # print_map(northers, easters, southers, westers)
+
+    print("Done")
     return 1
 
 
