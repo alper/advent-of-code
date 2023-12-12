51848d0439a49f4a0879d99f2075197bf1bfc8a7
Alper Çugun <github@alper.nl>
2023-12-13 00:15:59 +0100

    Cribbed a solution because mine hung

--- 2023/day-12/solution.py
+++ 2023/day-12/solution.py
@@ -1,5 +1,6 @@
 from collections import deque
-from typing import Deque
+from typing import Deque, List, Tuple
+import functools
 
 sample_input = """???.### 1,1,3
 .??..??...?##. 1,1,3
@@ -9,73 +10,59 @@ sample_input = """???.### 1,1,3
 ?###???????? 3,2,1"""
 
 real_input = sample_input
-# real_input = open("input.txt").read()
+real_input = open("input.txt").read()
 
-def check_possible(original: str, fixed: str, rest: str, conditions: Deque[int]):
-    # print(f"Fixed {fixed} open {rest}, conditions {conditions}")
+@functools.lru_cache(maxsize=None)
+def check_possible(unknown: str, damaged: Tuple[int]) -> int:
+    count = 0
 
-    conditions = conditions.copy()
+    space = sum(damaged)
+    limit = len(unknown) - space
+    span = damaged[0]
 
-    if not conditions:
-        # We have placed all conditions, now we just need to check that the rest of the segment does not have damaged springs
-        if '#' in rest:
-            return
+    for i in range(limit+1):
+        if i > 0 and unknown[i-1] == '#':
+            break
 
-        fixed += len(rest) * '.'
+        if all([c != '.' for c in unknown[i:i+span]]):
+            if len(damaged) == 1:
+                if all([c != '#' for c in unknown[i+span:]]):
+                    count += 1
+            elif (i+span == len(unknown) or unknown[i+span] != '#') and len(unknown) > i + space:
+                count += check_possible(unknown[i+span+1:], damaged[1:])
 
-        for i, c in enumerate(original):
-            if c == '#' and fixed[i] != '#':
-                return
-
-        print(f"Found a possible solution: {fixed}")
-        return
-
-    if sum(conditions) > len(rest):
-        return
-
-    damaged_count = conditions.popleft()
-
-    for i in range(len(rest)):
-        chunk = rest[i:i+damaged_count]
-
-        # Name the position indexes
-        chunk_start = i
-        chunk_end = i + damaged_count - 1
-
-        chunk_after = None
-        if chunk_end < len(rest) - 1:
-            chunk_after = rest[chunk_end+1]
-
-        if chunk.replace('?', '#') == "#" * damaged_count:
-            if chunk_after == None:
-                check_possible(original, fixed + ('#' * damaged_count), rest[chunk_end+1:], conditions)
-
-            if chunk_after == "?" or chunk_after == ".":
-                check_possible(original, fixed + ('#' * damaged_count) + '.', rest[chunk_end+2:], conditions)
-
-            if chunk_after == "#":
-                pass
-        fixed += '.'
-    return
+    return count
 
 
-def test_spring_possibilities():
-    assert spring_possibilities("???.### 1,1,3") == 1
-    assert spring_possibilities(".??..??...?##. 1,1,3") == 4
-    assert spring_possibilities("?###???????? 3,2,1") == 10
 
-# for line in real_input.splitlines():
-#     springs, broken = line.split()
-#     broken = [int(i) for i in broken.split(',')]
 
-#     check_possible(springs, "", springs, deque(broken))
+s1 = 0
+for line in real_input.splitlines():
+    springs, broken = line.split()
+    broken = [int(i) for i in broken.split(',')]
+
+    print("Checking:", springs, broken)
+    # s1 += check_possible(springs, "", springs, tuple(broken))
+
+    c = check_possible(springs, tuple(broken))
+    print("Possibilities:", c, "\n")
+    s1 += c
+
+print("Sum 1:", s1)
 
 # Part 2
 
+s2 = 0
 for line in real_input.splitlines():
     springs, broken = line.split()
     springs = '?'.join([springs]*5)
 
     broken = [int(i) for i in broken.split(',')] * 5
 
-    check_possible(springs, "", springs, deque(broken))
+    print("Checking:", springs, broken)
+    possibilities = check_possible(springs, tuple(broken))
+
+    print("Possibilities:", possibilities, "\n")
+    s2 += possibilities
+
+print("Sum 2:", s2)
